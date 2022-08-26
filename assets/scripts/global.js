f96f4e59cf71c3c8bd55613cdc47adfadb0c2ed9
rihan_a <a.rihan@live.com>
2022-08-26 13:16:35 +0200

    Testing a fix for the Hamburger Btn on IOS

--- assets/scripts/global.js
+++ assets/scripts/global.js
@@ -17,12 +17,12 @@ function showHideMenu() {
     menuLeft.style.display = "none";
     menuRight.style.display = "none";
     hamburgerMenu.value = "closed";
-    hamburgerMenu.innerHTML = `<i class="fi fi-bs-menu-burger"></i>`;
+    hamburgerMenu.innerHTML = '<i class="fi fi-bs-menu-burger"></i>';
   } else {
     menuLeft.style.display = "unset";
     menuRight.style.display = "unset";
     hamburgerMenu.value = "opened";
-    hamburgerMenu.innerHTML = `<i class="fi fi-bs-cross"></i>`;
+    hamburgerMenu.innerHTML = '<i class="fi fi-bs-cross"></i>';
   }
 }
 

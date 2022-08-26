1f58d94c74a1cc7415d7740f1b06758844659d39
rihan_a <a.rihan@live.com>
2022-08-26 20:55:53 +0200

    UX Update for the Add to cart btn

--- assets/scripts/addToCart.js
+++ assets/scripts/addToCart.js
@@ -1,8 +1,11 @@
-//import { renderSubtotal } from "./cart.js";
 import { getProducts } from "./fetchProducts.js";
 import { updateCartCount } from "./updateCartCount.js";
+// import { addedToCart } from "./shop.js";
+// import { addToCartIcon } from "./shop.js";
 
-// Check if there is products in cart or if empty intiate an empty array
+// Variables
+
+// Check if there is products in cart already or if it's empty intiate an empty array
 if (localStorage.getItem("cart")) {
   var cart = JSON.parse(localStorage.getItem("cart"));
 } else {
@@ -12,20 +15,28 @@ if (localStorage.getItem("cart")) {
 // Function to check which product is being added to the cart and push to the cart array
 export async function addToCart(theID) {
   let productsJSON = await getProducts(); // Products JSON
-
+  let modifiedProductsList = [];
   productsJSON.products.forEach(function (item) {
-    if (item.id == theID) {
-      let clickedProduct = item;
-      if (cart.some((item) => item.id == theID)) {
-        alert("Product already in the cart.");
-      } else {
-        cart.push({
-          ...clickedProduct,
-          numberOfUnits: 1,
+    modifiedProductsList.push({
+      ...item,
+      numberOfUnits: 0,
+    });
+  });
+
+  modifiedProductsList.forEach(function (theItem) {
+    if (theItem.id == theID) {
+      if (cart.some((cartItem) => cartItem.id == theID)) {
+        cart.some((cartItem) => {
+          if (cartItem.id == theID) {
+            cartItem.numberOfUnits++;
+          }
         });
+      } else {
+        cart.push({ ...theItem, numberOfUnits: 1 });
       }
     }
   });
+
   localStorage.setItem("cart", JSON.stringify(cart));
   updateCartCount();
   console.log("cart updated");
